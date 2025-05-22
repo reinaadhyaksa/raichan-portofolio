5ddd1e84a80601b83004f8d0e32b815ee49ab401
Reina Adhyaksa <violetevargarde@gmail.com>
2025-05-22 15:09:39 +0700

    raichan

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+import Main from "@/components/Main";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+}
